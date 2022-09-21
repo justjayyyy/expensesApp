2399bca642bc0b37f1eced8f32a9be5235399505
Jay <meresidijenia94@gmail.com>
2022-09-21 17:12:34 +0300

    feat: add router and home component

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+
+const AppRouter = () => (
+  <Router>
+    <Routes>
+      <Route path='/' exact element={ <Home/> }/>
+    </Routes>
+  </Router>
+)
+export default AppRouter;
